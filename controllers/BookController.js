5d8e00b664de559c94e800be1978a99612b6ee32
Amaya Lucas <amayajlucas@gmail.com>
2023-10-08 22:53:23 -0400

    route and controller updates

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -2,54 +2,54 @@ const { Book, ReadingList } = require('../models')
 
 // temporary for testing - remove when incorporating auth
 const createBook = async (req, res) => {
-    try {
-        const book = await new Book(req.body)
-        await book.save()
-        res.send(book)
-    } catch (error) {
-        throw error
-    }
+  try {
+    const book = await new Book(req.body)
+    await book.save()
+    res.send(book)
+  } catch (error) {
+    throw error
+  }
 }
 
 const getAllBooks = async (req, res) => {
-    try {
-        const books = await Book.findAll()
-        res.send(books)
-    } catch (error) {
-        throw error
-    }
+  try {
+    const books = await Book.findAll()
+    res.send(books)
+  } catch (error) {
+    throw error
+  }
 }
 
 const getBookById = async (req, res) => {
-    try {
-        const { book_id } = req.params
-        const book = await Book.findByPk(book_id)
-        res.send(book)
-    } catch (error) {
-        throw error
-    }
+  try {
+    const { book_id } = req.params
+    const book = await Book.findByPk(book_id)
+    res.send(book)
+  } catch (error) {
+    throw error
+  }
 }
 
 const deleteBook = async (req, res) => {
-    try {
-        const readingLists = await ReadingList.findAll({
-            where: { bookId: req.params.book_id}
-        })
-        await readingLists.map((list) => list.destroy())
-        await Book.destroy({ where: { id: req.params.book_id}})
-        res.send({
-            msg: 'Book Deleted',
-            payload: req.params.book_id,
-            status: 'Ok'
-        })
-    } catch (error) {
-        throw error
-    }
+  try {
+    const readingLists = await ReadingList.findAll({
+      where: { bookId: req.params.book_id }
+    })
+    await readingLists.map((list) => list.destroy())
+    await Book.destroy({ where: { id: req.params.book_id } })
+    res.send({
+      msg: 'Book Deleted',
+      payload: req.params.book_id,
+      status: 'Ok'
+    })
+  } catch (error) {
+    throw error
+  }
 }
 
 module.exports = {
-    createBook, 
-    getAllBooks,
-    getBookById,
-    deleteBook
+  createBook,
+  getAllBooks,
+  getBookById,
+  deleteBook
 }
--- controllers/ClubController.js
+++ controllers/ClubController.js
@@ -1,113 +1,118 @@
 const { Club, MemberList, User, ReadingList, Book } = require('../models')
 
 const createClub = async (req, res) => {
-    try {
-        const club = await new Club(req.body)
-        await club.save()
-        res.send(club)
-    } catch (error) {
-        throw error
-    }
+  try {
+    const club = await new Club(req.body)
+    await club.save()
+    res.send(club)
+  } catch (error) {
+    throw error
+  }
 }
 
 const getAllClubs = async (req, res) => {
-    try {
-        const clubs = await Club.findAll()
-        res.send(clubs)
-    } catch (error) {
-        throw error
-    }
+  try {
+    const clubs = await Club.findAll({
+      include: [
+        {
+          model: Book,
+          as: 'books',
+          through: ReadingList,
+          attributes: ['id', 'data']
+        }
+      ]
+    })
+    res.send(clubs)
+  } catch (error) {
+    throw error
+  }
 }
 
 const getClubById = async (req, res) => {
-    try {
-        const { club_id } = req.params
-        const club = await Club.findByPk(club_id, {
-            include: [
-                {
-                    model: Book,
-                    as: 'books',
-                    through: ReadingList,
-                    attributes: ['id','data']
-                }
-            ]
-        })
-        res.send(club)
-    } catch (error) {
-        throw error
-    }
+  try {
+    const { club_id } = req.params
+    const club = await Club.findByPk(club_id, {
+      include: [
+        {
+          model: Book,
+          as: 'books',
+          through: ReadingList,
+          attributes: ['id', 'data']
+        }
+      ]
+    })
+    res.send(club)
+  } catch (error) {
+    throw error
+  }
 }
 
 const addMemberToClub = async (req, res) => {
-    try {
-        const club = await Club.findByPk(req.params.club_id)
-        await club.addMembers([req.body.userId])
-        await club.save()
-        const response = await Club.findByPk(req.params.club_id, {
-            include: [
-                {
-                    model: User,
-                    through: MemberList,
-                    as: "members",
-                    attributes: ['id', 'firstName', 'lastName']
-                }
-            ]
-        })
-        res.send(response)
-    } catch (error) {
-        throw error
-    }
+  try {
+    const club = await Club.findByPk(req.params.club_id)
+    await club.addMembers([req.body.userId])
+    await club.save()
+    const response = await Club.findByPk(req.params.club_id, {
+      include: [
+        {
+          model: User,
+          through: MemberList,
+          as: 'members',
+          attributes: ['id', 'firstName', 'lastName']
+        }
+      ]
+    })
+    res.send(response)
+  } catch (error) {
+    throw error
+  }
 }
 
 const addBookToList = async (req, res) => {
-    try {
-        const club = await Club.findByPk(req.params.club_id)
-        await club.addBooks([req.body.bookId])
-        await club.save()
-        const response = await Club.findByPk(req.params.club_id, {
-            include: [
-                {
-                    model: Book,
-                    through: ReadingList,
-                    as: "books",
-                    attributes: ['id']
-                }
-            ]
-        })
-        res.send(response)
-    } catch (error) {
-        throw error
-    }
+  try {
+    const club = await Club.findByPk(req.params.club_id)
+    await club.addBooks([req.body.bookId])
+    await club.save()
+    const response = await Club.findByPk(req.params.club_id, {
+      include: [
+        {
+          model: Book,
+          through: ReadingList,
+          as: 'books',
+          attributes: ['id']
+        }
+      ]
+    })
+    res.send(response)
+  } catch (error) {
+    throw error
+  }
 }
 
 const removeBookFromList = async (req, res) => {
-    try {
-        const readingList = await ReadingList.findAll({
-            where: { 
-                bookId: req.params.book_id,
-                clubId: req.params.club_id
-            }
-        })
-        // await readingLists.map((list) => list.destroy())
-        // res.send({
-        //     msg: 'Book Removed From List',
-        //     payload: req.params.book_id,
-        //     status: 'Ok'
-        // })
-        res.send({
-            msg: 'List',
-            payload: readingList
-        })
-    } catch (error) {
-        throw error
-    }
+  try {
+    const readingList = await ReadingList.findAll({
+      where: {
+        bookId: req.params.book_id,
+        clubId: req.params.club_id
+      }
+    })
+    await readingList.map((list) => list.destroy())
+    res.send({
+      msg: 'Book Removed From List',
+      payload: req.params.book_id,
+      status: 'Ok'
+    })
+  } catch (error) {
+    throw error
+  }
 }
 
 module.exports = {
-    createClub,
-    getAllClubs,
-    getClubById,
-    addMemberToClub,
-    addBookToList,
-    removeBookFromList
+  createClub,
+  getAllClubs,
+  getClubById,
+  addMemberToClub,
+  addBookToList,
+  removeBookFromList
 }
